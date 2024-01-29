85eeb39befdee63a18acb58ad0beb12ded1e3527
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-01-29 12:51:37 -0500

    auto-push cleaned `dev` to `main` every push

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# A script to copy the actually-for-the-template files to the `template`
+# directory, to get it ready for the `publish.yml` GitHub workflow.
+
+# Making `dist` do double-duty for Astro & for this, because:
+#
+# 1. this process isn't using `dist`
+# 2. we don't want to clutter `.gitignore` with stuff that makes no sense in
+#    the context of the template
+rm -rf dist
+mkdir -p dist
+
+# Copy desired files into `dist`
+rsync -av --progress .vscode .env.example .gitattributes .gitignore * \
+  --exclude="dist" \
+  --exclude="contracts" \
+  --exclude="LICENSE" \
+  --exclude="publish.sh" \
+  --exclude="node_modules" \
+  --exclude="target" \
+  dist
