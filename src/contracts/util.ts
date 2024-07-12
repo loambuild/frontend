5ff8a753fc2eaddc63c761df081d3856ad0cb685
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-07-12 09:57:30 -0400

    fix: update for latest stellar-cli & loam-cli
    Add additional layout styling
    
    Update contract import on index page
    
    Update .env.example to allow for env vars to be available in Astro frontend files
    
    Add glob dev dep
    
    Update initialize.js
    
    Add contracts/utils.ts file
    
    Install loam-cli on npm install
    
    Update dev config in environments.toml
    
    Add staging and prod env config
    
    Don't install loam-cli globally

--- src/contracts/util.ts
+++ src/contracts/util.ts
@@ -1,2 +1,5 @@
-export const rpcUrl = import.meta.env.SOROBAN_RPC_URL ?? "http://localhost:8000/";
-export const networkPassphrase = import.meta.env.SOROBAN_NETWORK_PASSPHRASE ?? "Standalone Network ; February 2017";
+export const rpcUrl =
+  import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "http://localhost:8000/rpc";
+export const networkPassphrase =
+  import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ??
+  "Standalone Network ; February 2017";
